45feb82882b3f1b06e41d74448dfb911cda9729d
Markus Fritsche <fritsche.markus@gmail.com>
2024-09-10 11:49:43 +0000

    Change hotkeys

--- src/keyboard.c
+++ src/keyboard.c
@@ -23,7 +23,7 @@
 
 hotkey_combo_t hotkeys[] = {
     /* Main keyboard switching hotkey */
-    {.modifier       = 0,
+    {.modifier       = KEYBOARD_MODIFIER_RIGHTCTRL,
      .keys           = {HOTKEY_TOGGLE},
      .key_count      = 1,
      .pass_to_os     = false,
